9e6ffc9b2c5e65db58060eee153d22f40fcf4131
CodaGott <talk2don90@gmail.com>
2021-03-24 14:45:18 +0100

    api starter files

---
+++ src/main/java/com/stykka/StykkaEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.stykka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StykkaEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StykkaEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stykka/StykkaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stykka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StykkaEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
